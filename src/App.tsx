da5ddf252d561e14c416680cec111809f379f2d2
MaysaB <49612167+MaysaB@users.noreply.github.com>
2024-08-02 11:07:05 +0100

    added interface to extend devicemotionevent

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
 import Grid from "./components/grid";
 
+interface DeviceMotionEventWithPermission extends DeviceMotionEvent {
+  requestPermission?: () => Promise<PermissionState>;
+}
+
+
 function App() {
   const requestMotionPermission = () => {
-    console.log('clicked')
-    DeviceMotionEvent.requestPermission()
-    .then(response => {
-        if (response == 'granted') {
-            window.addEventListener('deviceorientation', (e) => {
-                // do something with e
-            })
-        }
-    })
-    .catch(console.error)
+    console.log('clicked');
+
+    const DeviceMotionEventWithPermission = DeviceMotionEvent as unknown as DeviceMotionEventWithPermission;
+
+    if (typeof DeviceMotionEventWithPermission.requestPermission === 'function') {
+      (DeviceMotionEventWithPermission).requestPermission()
+        .then(response => {
+          if (response === 'granted') {
+            window.addEventListener('deviceorientation', () => {
+              alert('Permissions granted');
+            });
+          }
+        })
+        .catch(console.error);
+    } else {
+      //browsers that don't support DeviceMotionEvent.requestPermission
+      console.warn('DeviceMotionEvent.requestPermission is not supported in this browser.');
+      alert('DeviceMotionEvent.requestPermission is not supported in this browser.');
+    }
 }
 
    return (
-    <><button 
+    <>
+    <main className="py-10 px-8 bg-blue-50 h-screen space-y-5 overflow-y-auto">
+      <button 
         onClick={requestMotionPermission} 
         className='font-bold text-center text-4xl text-orange-500'
       >
         Request Motion Permission
       </button>
-    <main className="py-10 px-8 bg-blue-50 h-screen space-y-5 overflow-y-auto">
-      
       <div className="min-h-screen flex items-center justify-center bg-blue-50">
       <Grid />
     </div>
